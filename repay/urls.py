34c4723c77f19785a34679c7b561c00265f79c06
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-09-13 21:44:37 +0300

    u

--- repay/urls.py
+++ repay/urls.py
@@ -2,8 +2,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("backend.urls"))
+    path("", include("backend.urls")),
+     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), 
+   
 ]
