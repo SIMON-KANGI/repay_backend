0a8cd6190fa3811d72de1725f9fe53f05e44159e
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-09-13 21:44:00 +0300

    user view

--- backend/views/userView.py
+++ backend/views/userView.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework import viewsets
-
-from backend.serializers import UserSerializer
+from rest_framework.authentication import TokenAuthentication 
+from backend.serializers.userSerializers import UserSerializer
 from backend.models.user import User
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class= UserSerializer
     permission_classes =[AllowAny]
+    authentication_classes = [TokenAuthentication]
+    
+    
